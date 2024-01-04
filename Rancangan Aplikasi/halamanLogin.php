99f80d999b026448ab8b094dd31fc2b2b0f2de84
aldovaferdiansyah <aldovaferdiansyah12@gmail.com>
2024-01-04 23:19:11 +0700

    Menambahkan rancangan awal halaman login

---
+++ Rancangan Aplikasi/halamanLogin.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Halaman Login</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>LOGIN SISTEM AKADEMIK</h1>
+        <form action="halamanDashboard">
+            <label><b> Username </b></label>
+            <input type="text" name="username" placeholder="Masukkan NIM atau NID" required="required">
+            <label><b> Password </b></label>
+            <input type="password" name="password" placeholder="Masukkan Password" required="required">
+            <br><br>
+            <button type="submit" class="btn btn-succes">Login</button>
+        </form>
+    </div>
+</body>
+</html>
